fae448bf5eb539379c9f9e660bcb7569d3d5dd37
YuraMakhnitko <bizoru82@gmail.com>
2023-02-26 11:09:56 +0200

    First-commit

---
+++ controllers/PostController.js
@@ -0,0 +1,150 @@
+import PostModel from '../models/Post.js';
+
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().limit(5).exec();
+    const tags = posts
+      .map((obj) => obj.tags)
+      .flat()
+      .slice(0, 5);
+
+    res.json(tags);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Can`t get articles',
+    });
+  }
+};
+
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find().populate('user').exec();
+    res.json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Can`t get articles',
+    });
+  }
+};
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: {
+          viewsCount: 1,
+        },
+      },
+      {
+        returnDocument: 'after',
+      },
+      (error, doc) => {
+        if (error) {
+          console.log(error);
+          return res.status(500).json({
+            message: 'Can`t return an article',
+          });
+        }
+        if (!doc) {
+          return res.status(404).json({
+            message: 'An article not found',
+          });
+        }
+        res.json(doc);
+      }
+    ).populate('user');
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Can`t get articles',
+    });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndDelete(
+      {
+        _id: postId,
+      },
+      (error, doc) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({
+            message: 'Can`t remove article',
+          });
+        }
+        if (!doc) {
+          return res.status(404).json({
+            message: 'An article not found',
+          });
+        }
+
+        res.json({
+          success: true,
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Can`t get articles',
+    });
+  }
+};
+
+export const create = async (req, res) => {
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags.split(','),
+      user: req.userId,
+    });
+
+    const post = await doc.save();
+
+    res.json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Can`t create article',
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    await PostModel.updateOne(
+      {
+        _id: postId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        user: req.userId,
+        tags: req.body.tags.split(','),
+      }
+    );
+    res.json({
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Can`t update article',
+    });
+  }
+};
